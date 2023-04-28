01aa6e853a6b87e9156a3a6271ccec3af93876a3
shuhai65 <shuhai.stack@gmail.com>
2023-04-28 13:08:14 +0800

    原型模式

---
+++ prototype/src/main/java/com/shuhai65/prototype/TopicRandomUtil.java
@@ -0,0 +1,32 @@
+package com.shuhai65.prototype;
+
+import java.util.*;
+
+public class TopicRandomUtil {
+
+    /**
+     * 乱序Map元素，记录对应答案key
+     * @param option 题目
+     * @param key    答案
+     * @return Topic 乱序后 {A=c., B=d., C=a., D=b.}
+     * 这个这个⼯具类的操作就是将原有Map中的选项乱序操作， 也就是A的选项内容给B ， B的可能给
+     * C ，同时记录正确答案在处理后的位置信息
+     */
+    static public Topic random(Map<String, String> option, String key) {
+        Set<String> keySet = option.keySet();
+        ArrayList<String> keyList = new ArrayList<String>(keySet);
+        Collections.shuffle(keyList);
+        HashMap<String, String> optionNew = new HashMap<String, String>();
+        int idx = 0;
+        String keyNew = "";
+        for (String next : keySet) {
+            String randomKey = keyList.get(idx++);
+            if (key.equals(next)) {
+                keyNew = randomKey;
+            }
+            optionNew.put(randomKey, option.get(next));
+        }
+        return new Topic(optionNew, keyNew);
+    }
+
+}
